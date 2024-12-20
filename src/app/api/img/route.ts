c735f68ff202ab642bd8f37acd1ee6094316c17a
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2024-12-21 03:03:04 +0600

    date sort

--- src/app/api/img/route.ts
+++ src/app/api/img/route.ts
@@ -4,13 +4,12 @@ import PhotosModel from "@/models/Photos";
 export async function POST(req: Request) {
   await dbConnect();
   const data = await req.json();
-  console.log(data);
   const photo = await PhotosModel.create(data);
   return Response.json(photo, { status: 201 });
 }
 
 export async function GET() {
   await dbConnect();
-  const photos = await PhotosModel.find({});
+  const photos = await PhotosModel.find({}).sort({ date: -1 });
   return Response.json(photos);
 }
